b2bad0c1cae110274e182e0aaba47004fd8611ff
Bryan Johns <73319490+johbry17@users.noreply.github.com>
2025-02-25 00:33:28 -0500

    feat: add lat/long in data_cleaning_v2

--- data_cleaning_v2.R
+++ data_cleaning_v2.R
@@ -81,30 +81,100 @@ q3_2019 <- convert_columns_to_character(q3_2019)
 q4_2019 <- convert_columns_to_character(q4_2019)
 
 # Stack individual quarter's data frames into one big data frame
-all_trips <- bind_rows(q1_2019, q2_2019, q3_2019, q4_2019, q1_2020)
+all_trips <- bind_rows(q1_2019, q2_2019, q3_2019, q4_2019)
 
 # Remove lat, long, birthyear, and gender fields as this data was dropped beginning in 2020
-all_trips <- all_trips %>%  
-  select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender,  "tripduration"))
-
-
-
+# all_trips <- all_trips %>%
+#   select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender,  "tripduration"))
+all_trips <- all_trips %>% 
+  select(-c("tripduration"))
 
 
 
 #======================================================
 # STEP 3: CLEAN UP AND ADD DATA TO PREPARE FOR ANALYSIS
 #======================================================
-# Inspect the new table that has been created
-colnames(all_trips)  #List of column names
-nrow(all_trips)  #How many rows are in data frame?
-dim(all_trips)  #Dimensions of the data frame?
-head(all_trips)  #See the first 6 rows of data frame.  Also tail(all_trips)
-str(all_trips)  #See list of columns and data types (numeric, character, etc)
-summary(all_trips)  #Statistical summary of data. Mainly for numerics
+# (3a) - Get station latitude and longitude
+# (3b) - Dry run with all_trips df of data cleaning / processing
+# (3c) - Function to clean / process each df (Q1, Q2, Q3, Q4)
+
+
+
+
+# (3a) - extract station lat / long from 2020 data
+
+# note that start and end stations are slightly different, so extract from both and merge
+start_stations_2020 <- q1_2020 %>%
+  select(start_station_id, latitude = start_lat, longitude = start_lng) %>%
+  distinct()
+
+end_stations_2020 <- q1_2020 %>%
+  select(end_station_id, latitude = end_lat, longitude = end_lng) %>%
+  distinct()
+
+# rename columns in end_stations_2020 to avoid conflicts
+end_stations_2020 <- end_stations_2020 %>%
+  rename(start_station_id = end_station_id)
+
+# merge the two data frames for a complete list of lat/long
+stations_2020 <- full_join(start_stations_2020, end_stations_2020, by = "start_station_id") %>%
+  mutate(
+    latitude = coalesce(latitude.x, latitude.y),
+    longitude = coalesce(longitude.x, longitude.y)
+  ) %>%
+  select(start_station_id, latitude, longitude) %>%
+  distinct()
+
+# ensure lat/long look like Chicago
+ggplot(stations_2020, aes(x = longitude, y = latitude)) +
+  geom_point() +
+  labs(title = "Scatter Plot of Station Locations",
+       x = "Longitude",
+       y = "Latitude") +
+  theme_minimal()
+
+# add start station latitude and longitude
+all_trips <- all_trips %>%
+  left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
+  rename(start_lat = latitude, start_lng = longitude)
+
+# add end station latitude and longitude
+all_trips <- all_trips %>%
+  left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
+  rename(end_lat = latitude, end_lng = longitude)
+
+# see which stations don't have lat / long
+na_start_stations <- all_trips %>%
+  select(start_station_id, start_station_name, start_lat, start_lng) %>%
+  filter(is.na(start_lat) | is.na(start_lng)) %>%
+  distinct()
+na_end_stations <- all_trips %>%
+  select(end_station_id, end_station_name, end_lat, end_lng) %>%
+  filter(is.na(end_lat) | is.na(end_lng)) %>%
+  distinct()
+na_stations <- bind_rows(
+  na_start_stations %>% rename(station_id = start_station_id, station_name = start_station_name, latitude = start_lat, longitude = start_lng),
+  na_end_stations %>% rename(station_id = end_station_id, station_name = end_station_name, latitude = end_lat, longitude = end_lng)
+) %>% 
+  distinct()
+# 11 stations without lat/long
+
+# count the number of rows in all_trips that lack latitude or longitude for either start or end stations
+na_count <- all_trips %>%
+  filter(is.na(start_lat) | is.na(start_lng) | is.na(end_lat) | is.na(end_lng)) %>%
+  nrow()
+print(paste("Number of rows with NA latitude or longitude:", na_count))
+# 366 rows, of 3,818,004. Not gonna worry about it.
+
+
+
+
+
+# (3b) - Dry run with all_trips df of data cleaning / processing, interleaved with Google's template
 
 # There are a few problems we will need to fix:
 # (1) In the "member_casual" column, there are two names for members ("member" and "Subscriber") and two names for casual riders ("Customer" and "casual"). We will need to consolidate that from four to two labels.
+#     (1a) This may be unnecessary, as I'm currently using 2019 data, but I prefer the 2020 names, and wish to be ready if I chose to add 2020 data in the future.
 # (2) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
 # (3) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
 # (4) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
@@ -135,7 +205,7 @@ all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
 
 # Add a "ride_length" calculation to all_trips (in seconds)
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/difftime.html
-all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
+all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at, units = "mins")
 
 # Inspect the structure of the columns
 str(all_trips)
@@ -154,10 +224,15 @@ all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$
 
 
 
+
+# (3c) - Function to clean / process each df (Q1, Q2, Q3, Q4)
+
 # Repeat, for individual Quarters, in case I want to analyze data by season (using Q1, 2, 3, and 4 as proxies for seasons)
 clean_data <- function(df) {
   # Define the columns to drop
-  columns_to_drop <- c("start_lat", "start_lng", "end_lat", "end_lng", "birthyear", "gender", "tripduration")
+  # for 2019, not planning on running 2020 through here
+  # columns_to_drop <- c("start_lat", "start_lng", "end_lat", "end_lng", "birthyear", "gender", "tripduration")
+  columns_to_drop <- c("tripduration")
   
   # Drop columns if they exist in the dataframe (to account for differences between 2019 and 2020)
   df <- df %>% select(-one_of(intersect(columns_to_drop, colnames(df))))
@@ -184,6 +259,17 @@ clean_data <- function(df) {
   # Remove rows with start_station_name "HQ QR" or negative ride_length
   df <- df[!(df$start_station_name == "HQ QR" | df$ride_length < 0),]
   
+  # add start station latitude and longitude
+  df <- df %>%
+    left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
+    rename(start_lat = latitude, start_lng = longitude)
+  
+  # add end station latitude and longitude
+  df <- df %>%
+    left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
+    rename(end_lat = latitude, end_lng = longitude)
+  
+  
   return(df)
 }
 
@@ -192,74 +278,74 @@ q1_2019_cleaned <- clean_data(q1_2019)
 q2_2019_cleaned <- clean_data(q2_2019)
 q3_2019_cleaned <- clean_data(q3_2019)
 q4_2019_cleaned <- clean_data(q4_2019)
-q1_2020_cleaned <- clean_data(q1_2020)
+q1_2020_cleaned <- q1_2020
 
 #=====================================
 # STEP 4: CONDUCT DESCRIPTIVE ANALYSIS
 #=====================================
-# Descriptive analysis on ride_length (all figures in seconds)
-mean(all_trips_v2$ride_length) #straight average (total ride length / rides)
-median(all_trips_v2$ride_length) #midpoint number in the ascending array of ride lengths
-max(all_trips_v2$ride_length) #longest ride
-min(all_trips_v2$ride_length) #shortest ride
-
-# You can condense the four lines above to one line using summary() on the specific attribute
-summary(all_trips_v2$ride_length)
-
-# Compare members and casual users
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = mean)
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
-
-# See the average ride time by each day for members vs casual users
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-
-# Notice that the days of the week are out of order. Let's fix that.
-all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
-
-# Now, let's run the average ride time by each day for members vs casual users
-aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-
-# analyze ridership data by type and weekday
-all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>%  #creates weekday field using wday()
-  group_by(member_casual, weekday) %>%  #groups by usertype and weekday
-  summarise(number_of_rides = n()							#calculates the number of rides and average duration 
-            ,average_duration = mean(ride_length)) %>% 		# calculates the average duration
-  arrange(member_casual, weekday)								# sorts
-
-# Let's visualize the number of rides by rider type
-p <- all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>% 
-  group_by(member_casual, weekday) %>% 
-  summarise(number_of_rides = n()
-            ,average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday)  %>% 
-  ggplot(aes(x = weekday, y = number_of_rides, fill = member_casual)) +
-  geom_col(position = "dodge") +
-  theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, hjust = 1))
-ggsave("./resources/images/rides_per_day.png", plot = p)
-
-# Let's create a visualization for average duration
-p <- all_trips_v2 %>% 
-  mutate(weekday = wday(started_at, label = TRUE)) %>% 
-  group_by(member_casual, weekday) %>% 
-  summarise(number_of_rides = n()
-            ,average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday)  %>% 
-  ggplot(aes(x = weekday, y = average_duration, fill = member_casual)) +
-  geom_col(position = "dodge") +
-  theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, hjust = 1))
-ggsave("./resources/images/average_duration.png", plot = p)
+# # Descriptive analysis on ride_length (all figures in seconds)
+# mean(all_trips_v2$ride_length) #straight average (total ride length / rides)
+# median(all_trips_v2$ride_length) #midpoint number in the ascending array of ride lengths
+# max(all_trips_v2$ride_length) #longest ride
+# min(all_trips_v2$ride_length) #shortest ride
+# 
+# # You can condense the four lines above to one line using summary() on the specific attribute
+# summary(all_trips_v2$ride_length)
+# 
+# # Compare members and casual users
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = mean)
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
+# 
+# # See the average ride time by each day for members vs casual users
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+# 
+# # Notice that the days of the week are out of order. Let's fix that.
+# all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+# 
+# # Now, let's run the average ride time by each day for members vs casual users
+# aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+# 
+# # analyze ridership data by type and weekday
+# all_trips_v2 %>% 
+#   mutate(weekday = wday(started_at, label = TRUE)) %>%  #creates weekday field using wday()
+#   group_by(member_casual, weekday) %>%  #groups by usertype and weekday
+#   summarise(number_of_rides = n()							#calculates the number of rides and average duration 
+#             ,average_duration = mean(ride_length)) %>% 		# calculates the average duration
+#   arrange(member_casual, weekday)								# sorts
+# 
+# # Let's visualize the number of rides by rider type
+# p <- all_trips_v2 %>% 
+#   mutate(weekday = wday(started_at, label = TRUE)) %>% 
+#   group_by(member_casual, weekday) %>% 
+#   summarise(number_of_rides = n()
+#             ,average_duration = mean(ride_length)) %>% 
+#   arrange(member_casual, weekday)  %>% 
+#   ggplot(aes(x = weekday, y = number_of_rides, fill = member_casual)) +
+#   geom_col(position = "dodge") +
+#   theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, hjust = 1))
+# ggsave("./resources/images/rides_per_day.png", plot = p)
+# 
+# # Let's create a visualization for average duration
+# p <- all_trips_v2 %>% 
+#   mutate(weekday = wday(started_at, label = TRUE)) %>% 
+#   group_by(member_casual, weekday) %>% 
+#   summarise(number_of_rides = n()
+#             ,average_duration = mean(ride_length)) %>% 
+#   arrange(member_casual, weekday)  %>% 
+#   ggplot(aes(x = weekday, y = average_duration, fill = member_casual)) +
+#   geom_col(position = "dodge") +
+#   theme(legend.position = "bottom", axis.text.x = element_text(angle = 90, hjust = 1))
+# ggsave("./resources/images/average_duration.png", plot = p)
 
 #=================================================
 # STEP 5: EXPORT SUMMARY FILE FOR FURTHER ANALYSIS
 #=================================================
 # Create a csv file that we will visualize in Excel, Tableau, or my presentation software
 # N.B.: This file location is for a Mac. If you are working on a PC, change the file location accordingly (most likely "C:\Users\YOUR_USERNAME\Desktop\...") to export the data. You can read more here: https://datatofish.com/export-dataframe-to-csv-in-r/
-counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-write.csv(counts, file = './resources/data/avg_ride_length_v2.csv')
+# counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+# write.csv(counts, file = './resources/data/avg_ride_length_v2.csv')
 write.csv(q1_2019_cleaned, file = './resources/data/q1_2019_cleaned.csv')
 write.csv(q2_2019_cleaned, file = './resources/data/q2_2019_cleaned.csv')
 write.csv(q3_2019_cleaned, file = './resources/data/q3_2019_cleaned.csv')
