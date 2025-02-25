4b59b3d750a203ecb5400dd26179d3cc5ece32fc
Bryan Johns <73319490+johbry17@users.noreply.github.com>
2025-02-25 01:34:45 -0500

    feat: add Chicago map, season, finalize data_cleaning_v2

--- data_cleaning_v2.R
+++ data_cleaning_v2.R
@@ -1,3 +1,10 @@
+#=====================
+# Note that many steps are commented out
+# Legacy steps from converting the Google template to the current version
+# Often concerned with testing, or other analysis steps
+# Left in case of future usefulness
+#=====================
+
 library(tidyverse)  #helps wrangle data
 # Use the conflicted package to manage conflicts
 library(conflicted)
@@ -60,34 +67,34 @@ q1_2019 <- rename_columns(q1_2019)
 q3_2019 <- rename_columns(q3_2019)
 q4_2019 <- rename_columns(q4_2019)
 
-# Inspect the dataframes and look for incongruencies
-str(q1_2019)
-str(q1_2020)
-
-# Convert ride_id and rideable_type to character so that they can stack correctly
-# q1_2019 <-  mutate(q1_2019, ride_id = as.character(ride_id)
-#                    ,rideable_type = as.character(rideable_type)) 
-convert_columns_to_character <- function(df) {
-  df <- mutate(df, 
-               ride_id = as.character(ride_id),
-               rideable_type = as.character(rideable_type))
-  return(df)
-}
-
-# Apply the function to each quarter's data frame
-q1_2019 <- convert_columns_to_character(q1_2019)
-q2_2019 <- convert_columns_to_character(q2_2019)
-q3_2019 <- convert_columns_to_character(q3_2019)
-q4_2019 <- convert_columns_to_character(q4_2019)
-
-# Stack individual quarter's data frames into one big data frame
-all_trips <- bind_rows(q1_2019, q2_2019, q3_2019, q4_2019)
-
-# Remove lat, long, birthyear, and gender fields as this data was dropped beginning in 2020
-# all_trips <- all_trips %>%
-#   select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender,  "tripduration"))
-all_trips <- all_trips %>% 
-  select(-c("tripduration"))
+# # Inspect the dataframes and look for incongruencies
+# str(q1_2019)
+# str(q1_2020)
+# 
+# # Convert ride_id and rideable_type to character so that they can stack correctly
+# # q1_2019 <-  mutate(q1_2019, ride_id = as.character(ride_id)
+# #                    ,rideable_type = as.character(rideable_type)) 
+# convert_columns_to_character <- function(df) {
+#   df <- mutate(df, 
+#                ride_id = as.character(ride_id),
+#                rideable_type = as.character(rideable_type))
+#   return(df)
+# }
+# 
+# # Apply the function to each quarter's data frame
+# q1_2019 <- convert_columns_to_character(q1_2019)
+# q2_2019 <- convert_columns_to_character(q2_2019)
+# q3_2019 <- convert_columns_to_character(q3_2019)
+# q4_2019 <- convert_columns_to_character(q4_2019)
+# 
+# # Stack individual quarter's data frames into one big data frame
+# all_trips <- bind_rows(q1_2019, q2_2019, q3_2019, q4_2019)
+# 
+# # Remove lat, long, birthyear, and gender fields as this data was dropped beginning in 2020
+# # all_trips <- all_trips %>%
+# #   select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender,  "tripduration"))
+# all_trips <- all_trips %>% 
+#   select(-c("tripduration"))
 
 
 
@@ -126,100 +133,130 @@ stations_2020 <- full_join(start_stations_2020, end_stations_2020, by = "start_s
   distinct()
 
 # ensure lat/long look like Chicago
-ggplot(stations_2020, aes(x = longitude, y = latitude)) +
-  geom_point() +
+
+# ggplot(stations_2020, aes(x = longitude, y = latitude)) +
+#   geom_point() +
+#   labs(title = "Scatter Plot of Station Locations",
+#        x = "Longitude",
+#        y = "Latitude") +
+#   theme_minimal()
+
+library(sf)
+library(ggspatial)
+
+# convert the stations_2020 data frame to an sf object
+stations_2020_sf <- stations_2020 %>% drop_na() %>% st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
+# plot the map with the station locations
+ggplot() +
+  annotation_map_tile(type = "osm", zoom = 12) +
+  geom_sf(data = stations_2020_sf, color = "red", size = 2) +
   labs(title = "Scatter Plot of Station Locations",
        x = "Longitude",
        y = "Latitude") +
   theme_minimal()
 
-# add start station latitude and longitude
-all_trips <- all_trips %>%
-  left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
-  rename(start_lat = latitude, start_lng = longitude)
+# # add start station latitude and longitude
+# all_trips <- all_trips %>%
+#   left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
+#   rename(start_lat = latitude, start_lng = longitude)
+# 
+# # add end station latitude and longitude
+# all_trips <- all_trips %>%
+#   left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
+#   rename(end_lat = latitude, end_lng = longitude)
 
-# add end station latitude and longitude
-all_trips <- all_trips %>%
-  left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
-  rename(end_lat = latitude, end_lng = longitude)
-
-# see which stations don't have lat / long
-na_start_stations <- all_trips %>%
-  select(start_station_id, start_station_name, start_lat, start_lng) %>%
-  filter(is.na(start_lat) | is.na(start_lng)) %>%
-  distinct()
-na_end_stations <- all_trips %>%
-  select(end_station_id, end_station_name, end_lat, end_lng) %>%
-  filter(is.na(end_lat) | is.na(end_lng)) %>%
-  distinct()
-na_stations <- bind_rows(
-  na_start_stations %>% rename(station_id = start_station_id, station_name = start_station_name, latitude = start_lat, longitude = start_lng),
-  na_end_stations %>% rename(station_id = end_station_id, station_name = end_station_name, latitude = end_lat, longitude = end_lng)
-) %>% 
-  distinct()
-# 11 stations without lat/long
-
-# count the number of rows in all_trips that lack latitude or longitude for either start or end stations
-na_count <- all_trips %>%
-  filter(is.na(start_lat) | is.na(start_lng) | is.na(end_lat) | is.na(end_lng)) %>%
-  nrow()
-print(paste("Number of rows with NA latitude or longitude:", na_count))
-# 366 rows, of 3,818,004. Not gonna worry about it.
-
-
+# # see which stations don't have lat / long
+# na_start_stations <- all_trips %>%
+#   select(start_station_id, start_station_name, start_lat, start_lng) %>%
+#   filter(is.na(start_lat) | is.na(start_lng)) %>%
+#   distinct()
+# na_end_stations <- all_trips %>%
+#   select(end_station_id, end_station_name, end_lat, end_lng) %>%
+#   filter(is.na(end_lat) | is.na(end_lng)) %>%
+#   distinct()
+# na_stations <- bind_rows(
+#   na_start_stations %>% rename(station_id = start_station_id, station_name = start_station_name, latitude = start_lat, longitude = start_lng),
+#   na_end_stations %>% rename(station_id = end_station_id, station_name = end_station_name, latitude = end_lat, longitude = end_lng)
+# ) %>% 
+#   distinct()
+# # 11 stations without lat/long
+# 
+# # count the number of rows in all_trips that lack latitude or longitude for either start or end stations
+# na_count <- all_trips %>%
+#   filter(is.na(start_lat) | is.na(start_lng) | is.na(end_lat) | is.na(end_lng)) %>%
+#   nrow()
+# print(paste("Number of rows with NA latitude or longitude:", na_count))
+# # 366 rows, of 3,818,004. Not gonna worry about it.
 
 
 
-# (3b) - Dry run with all_trips df of data cleaning / processing, interleaved with Google's template
 
-# There are a few problems we will need to fix:
-# (1) In the "member_casual" column, there are two names for members ("member" and "Subscriber") and two names for casual riders ("Customer" and "casual"). We will need to consolidate that from four to two labels.
-#     (1a) This may be unnecessary, as I'm currently using 2019 data, but I prefer the 2020 names, and wish to be ready if I chose to add 2020 data in the future.
-# (2) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
-# (3) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
-# (4) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
+# helper function to set season
+get_season <- function(month) {
+  if (month %in% c("12", "01", "02")) {
+    return("Winter")
+  } else if (month %in% c("03", "04", "05")) {
+    return("Spring")
+  } else if (month %in% c("06", "07", "08")) {
+    return("Summer")
+  } else if (month %in% c("09", "10", "11")) {
+    return("Fall")
+  }
+}
 
-# In the "member_casual" column, replace "Subscriber" with "member" and "Customer" with "casual"
-# Before 2020, Divvy used different labels for these two types of riders ... we will want to make our dataframe consistent with their current nomenclature
-# N.B.: "Level" is a special property of a column that is retained even if a subset does not contain any values from a specific level
-# Begin by seeing how many observations fall under each usertype
-table(all_trips$member_casual)
-
-# Reassign to the desired values (we will go with the current 2020 labels)
-all_trips <-  all_trips %>% 
-  mutate(member_casual = recode(member_casual
-                                ,"Subscriber" = "member"
-                                ,"Customer" = "casual"))
-
-# Check to make sure the proper number of observations were reassigned
-table(all_trips$member_casual)
-
-# Add columns that list the date, month, day, and year of each ride
-# This will allow us to aggregate ride data for each month, day, or year ... before completing these operations we could only aggregate at the ride level
-# https://www.statmethods.net/input/dates.html more on date formats in R found at that link
-all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd
-all_trips$month <- format(as.Date(all_trips$date), "%m")
-all_trips$day <- format(as.Date(all_trips$date), "%d")
-all_trips$year <- format(as.Date(all_trips$date), "%Y")
-all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
-
-# Add a "ride_length" calculation to all_trips (in seconds)
-# https://stat.ethz.ch/R-manual/R-devel/library/base/html/difftime.html
-all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at, units = "mins")
-
-# Inspect the structure of the columns
-str(all_trips)
-
-# Convert "ride_length" from Factor to numeric so we can run calculations on the data
-is.factor(all_trips$ride_length)
-all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
-is.numeric(all_trips$ride_length)
-
-# Remove "bad" data
-# The dataframe includes a few hundred entries when bikes were taken out of docks and checked for quality by Divvy or ride_length was negative
-# We will create a new version of the dataframe (v2) since data is being removed
-# https://www.datasciencemadesimple.com/delete-or-drop-rows-in-r-with-conditions-2/
-all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
+# # (3b) - Dry run with all_trips df of data cleaning / processing, interleaved with Google's template
+# 
+# # There are a few problems we will need to fix:
+# # (1) In the "member_casual" column, there are two names for members ("member" and "Subscriber") and two names for casual riders ("Customer" and "casual"). We will need to consolidate that from four to two labels.
+# #     (1a) This may be unnecessary, as I'm currently using 2019 data, but I prefer the 2020 names, and wish to be ready if I chose to add 2020 data in the future.
+# # (2) The data can only be aggregated at the ride-level, which is too granular. We will want to add some additional columns of data -- such as day, month, year -- that provide additional opportunities to aggregate the data.
+# # (3) We will want to add a calculated field for length of ride since the 2020Q1 data did not have the "tripduration" column. We will add "ride_length" to the entire dataframe for consistency.
+# # (4) There are some rides where tripduration shows up as negative, including several hundred rides where Divvy took bikes out of circulation for Quality Control reasons. We will want to delete these rides.
+# 
+# # In the "member_casual" column, replace "Subscriber" with "member" and "Customer" with "casual"
+# # Before 2020, Divvy used different labels for these two types of riders ... we will want to make our dataframe consistent with their current nomenclature
+# # N.B.: "Level" is a special property of a column that is retained even if a subset does not contain any values from a specific level
+# # Begin by seeing how many observations fall under each usertype
+# table(all_trips$member_casual)
+# 
+# # Reassign to the desired values (we will go with the current 2020 labels)
+# all_trips <-  all_trips %>% 
+#   mutate(member_casual = recode(member_casual
+#                                 ,"Subscriber" = "member"
+#                                 ,"Customer" = "casual"))
+# 
+# # Check to make sure the proper number of observations were reassigned
+# table(all_trips$member_casual)
+# 
+# # Add columns that list the date, month, day, and year of each ride
+# # This will allow us to aggregate ride data for each month, day, or year ... before completing these operations we could only aggregate at the ride level
+# # https://www.statmethods.net/input/dates.html more on date formats in R found at that link
+# all_trips$date <- as.Date(all_trips$started_at) #The default format is yyyy-mm-dd
+# all_trips$month <- format(as.Date(all_trips$date), "%m")
+# all_trips$day <- format(as.Date(all_trips$date), "%d")
+# all_trips$year <- format(as.Date(all_trips$date), "%Y")
+# all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
+# all_trips$season <- sapply(all_trips$month, get_season)
+# 
+# 
+# # Add a "ride_length" calculation to all_trips (in seconds)
+# # https://stat.ethz.ch/R-manual/R-devel/library/base/html/difftime.html
+# all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at, units = "mins")
+# 
+# # Inspect the structure of the columns
+# str(all_trips)
+# 
+# # Convert "ride_length" from Factor to numeric so we can run calculations on the data
+# is.factor(all_trips$ride_length)
+# all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
+# is.numeric(all_trips$ride_length)
+# 
+# # Remove "bad" data
+# # The dataframe includes a few hundred entries when bikes were taken out of docks and checked for quality by Divvy or ride_length was negative
+# # We will create a new version of the dataframe (v2) since data is being removed
+# # https://www.datasciencemadesimple.com/delete-or-drop-rows-in-r-with-conditions-2/
+# all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
 
 
 
@@ -230,8 +267,9 @@ all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$
 # Repeat, for individual Quarters, in case I want to analyze data by season (using Q1, 2, 3, and 4 as proxies for seasons)
 clean_data <- function(df) {
   # Define the columns to drop
-  # for 2019, not planning on running 2020 through here
+  # alternate versions depend on what I keep and add
   # columns_to_drop <- c("start_lat", "start_lng", "end_lat", "end_lng", "birthyear", "gender", "tripduration")
+  # columns_to_drop <- c("start_lat", "start_lng", "end_lat", "end_lng", "tripduration")
   columns_to_drop <- c("tripduration")
   
   # Drop columns if they exist in the dataframe (to account for differences between 2019 and 2020)
@@ -246,6 +284,7 @@ clean_data <- function(df) {
   df$day <- format(as.Date(df$date), "%d")
   df$year <- format(as.Date(df$date), "%Y")
   df$day_of_week <- format(as.Date(df$date), "%A")
+  df$season <- sapply(df$month, get_season)
   
   # Add ride_length column, converting data to minutes
   # 2019 was in mins, 2020 in secs - combining to all_trips above must've forced calculations to seconds
@@ -260,14 +299,18 @@ clean_data <- function(df) {
   df <- df[!(df$start_station_name == "HQ QR" | df$ride_length < 0),]
   
   # add start station latitude and longitude
-  df <- df %>%
-    left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
-    rename(start_lat = latitude, start_lng = longitude)
+  if (!("start_lat" %in% colnames(df) & "start_lng" %in% colnames(df))) {
+    df <- df %>%
+      left_join(stations_2020, by = c("start_station_id" = "start_station_id")) %>%
+      rename(start_lat = latitude, start_lng = longitude)
+  }
   
   # add end station latitude and longitude
-  df <- df %>%
-    left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
-    rename(end_lat = latitude, end_lng = longitude)
+  if (!("end_lat" %in% colnames(df) & "end_lng" %in% colnames(df))) {
+    df <- df %>%
+      left_join(stations_2020, by = c("end_station_id" = "start_station_id")) %>%
+      rename(end_lat = latitude, end_lng = longitude)
+  }
   
   
   return(df)
@@ -278,7 +321,8 @@ q1_2019_cleaned <- clean_data(q1_2019)
 q2_2019_cleaned <- clean_data(q2_2019)
 q3_2019_cleaned <- clean_data(q3_2019)
 q4_2019_cleaned <- clean_data(q4_2019)
-q1_2020_cleaned <- q1_2020
+q1_2020_cleaned <- clean_data(q1_2020)
+  
 
 #=====================================
 # STEP 4: CONDUCT DESCRIPTIVE ANALYSIS
